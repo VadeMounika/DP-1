49976b598cde0aed45dfef8ad6ec1c80bcfa0188
nandu070 <69900975+VadeMounika@users.noreply.github.com>
2025-03-25 10:55:58 -0700

    DP solution for painting houses completed

--- solution.java
+++ solution.java
@@ -44,3 +44,34 @@ class Solution {
 
     }
 }
+
+class Solution {
+    public int minCost(int[][] costs) {
+
+
+        int[][] dp = new int[costs.length][3];
+        dp = costs;
+
+        for(int i = costs.length -2; i >=0; i--)
+        {
+            dp[i][0] = Math.min(
+                    (dp[i+1][1]+dp[i][0]),
+                    (dp[i][0] + dp[i+1][2])
+            );
+            dp[i][1] = Math.min(
+                    (dp[i+1][0]+dp[i][1]),
+                    (dp[i][1] + dp[i+1][2])
+            );
+            dp[i][2] = Math.min(
+                    (dp[i+1][0]+dp[i][2]),
+                    (dp[i][2] + dp[i+1][1])
+            );
+
+        }
+
+        return Math.min(
+                Math.min(dp[0][0], dp[0][1]),
+                Math.min(dp[0][1], dp[0][2])
+        );
+    }
+}
